0df59c606db2347ba5045de15106c8e06e4eac75
Ubaid Ahmad <bhatubaid341@gmail.com>
2026-09-23 03:28:49 +0530

    style: scale logo up without affecting nav height

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -67,7 +67,7 @@ export const Navbar = () => {
                 alt="The Indian Wings Logo"
                 width={500}
                 height={293}
-                className={`object-contain w-auto hover:scale-105 transition-all duration-300 origin-left ${
+                className={`object-contain w-auto scale-[1.35] sm:scale-[1.45] lg:scale-[1.55] xl:scale-[1.65] hover:scale-[1.4] sm:hover:scale-[1.5] lg:hover:scale-[1.6] xl:hover:scale-[1.7] transition-all duration-300 origin-left ${
                   isScrolled ? 'h-8 md:h-9 xl:h-9.5' : 'h-8.5 md:h-9.5 xl:h-10'
                 }`} 
                 loading="eager"
